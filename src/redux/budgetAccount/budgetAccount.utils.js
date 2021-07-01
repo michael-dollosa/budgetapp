130b2334064f9d33cea0c3b596ede52189ff4a59
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-07-01 19:50:18 +0800

    modified logic for updateexpense, and update current balance for all reducers

--- src/redux/budgetAccount/budgetAccount.utils.js
+++ src/redux/budgetAccount/budgetAccount.utils.js
@@ -8,18 +8,13 @@ const removeItemFromArray = (transactionArray, transactionIdToDelete) => {
   })
 }
 
-
 const updateIncome = (transactionArray, transactionItem, actionType) => {
-  // if(transactionItem.type === "income") return transactionItem.cost
-  // else return 0
-  console.log("ACTION TYPE", actionType)
   switch (actionType) {
     case "ADD_TRANSACTION":{
       //add first the new item to the current array of transaction
       const transactions = tempAddTransactionToArray(transactionArray, transactionItem)
       //return array of transaction type = income
       const incomeTransactions = transactions.filter((transaction) => transaction.type === "income")
-      console.log("ADD TRANSACTION", incomeTransactions)
       //use reduce to return total Income
       const totalIncome =  incomeTransactions.reduce((accumulator, item) => {
         return(accumulator + item.cost)
@@ -30,10 +25,8 @@ const updateIncome = (transactionArray, transactionItem, actionType) => {
     case "DELETE_TRANSACTION":{
       //add first the new item to the current array of transaction
       const transactions = removeItemFromArray(transactionArray, transactionItem)
-      
       //return array of transaction type = income
       const incomeTransactions = transactions.filter((transaction) => transaction.type === "income")
-      console.log("DELETE TRANSACTION", incomeTransactions)
       //use reduce to return total Income
       const totalIncome =  incomeTransactions.reduce((accumulator, item) => {
         return(accumulator + item.cost)
@@ -45,17 +38,66 @@ const updateIncome = (transactionArray, transactionItem, actionType) => {
   }
   
 }
-const updateExpense = (transactionItem) => {
-  if(transactionItem.type === "expense") return transactionItem.cost
-  else return 0
+const updateExpense = (transactionArray, transactionItem, actionType) => {
+  switch (actionType) {
+    case "ADD_TRANSACTION":{
+      //add first the new item to the current array of transaction
+      const transactions = tempAddTransactionToArray(transactionArray, transactionItem)
+      //return array of transaction type = income
+      const incomeTransactions = transactions.filter((transaction) => transaction.type === "expense")
+      //use reduce to return total Income
+      const totalExpense =  incomeTransactions.reduce((accumulator, item) => {
+        return(accumulator + item.cost)
+      }, 0)
+      return totalExpense
+    }
+
+    case "DELETE_TRANSACTION":{
+      //add first the new item to the current array of transaction
+      const transactions = removeItemFromArray(transactionArray, transactionItem)
+      //return array of transaction type = income
+      const incomeTransactions = transactions.filter((transaction) => transaction.type === "expense")
+      //use reduce to return total Income
+      const totalExpense =  incomeTransactions.reduce((accumulator, item) => {
+        return(accumulator + item.cost)
+      }, 0)
+      return totalExpense
+    }
+    default:
+      break;
+  }
+}
+
+const updateBalance2 = (transactionArray, transactionItem, actionType) => {
+  switch (actionType) {
+    case "ADD_TRANSACTION":{
+      //add first the new item to the current array of transaction
+      const transactions = tempAddTransactionToArray(transactionArray, transactionItem)
+      //use reduce to return current Balance
+      const currentBalance =  transactions.reduce((accumulator, item) => {
+        return(accumulator + item.cost)
+      }, 0)
+      return currentBalance
+    }
+
+    case "DELETE_TRANSACTION":{
+      //add first the new item to the current array of transaction
+      const transactions = removeItemFromArray(transactionArray, transactionItem)
+      //use reduce to return total Income
+      const currentBalance =  transactions.reduce((accumulator, item) => {
+        return(accumulator + item.cost)
+      }, 0)
+      return currentBalance
+    }
+    default:
+      break;
+  }
 }
 
 const updateBalance = (transactionArray, transactionItem, currentBalance) => {
   const totalExpense =  transactionArray.reduce((accumulator, item) => {
     return(accumulator + item.cost)
   }, transactionItem.cost)
-  // console.log("update balance", currentBalance)
-  // console.log("update balance -total expense", totalExpense)
   return totalExpense
 }
 
@@ -73,8 +115,9 @@ export const addTransaction = (budgetState, transactionToAdd) => {
       transactions: [...account.transactions, transactionToAdd],
       // totalIncome: account.totalIncome + Number(updateIncome(transactionToAdd)),
       totalIncome: updateIncome(account.transactions, transactionToAdd, "ADD_TRANSACTION"),
-      totalExpense: account.totalExpense + Number(updateExpense(transactionToAdd)),
-      currentBalance: updateBalance(account.transactions, transactionToAdd, account.currentBalance)
+      // totalExpense: account.totalExpense + Number(updateExpense(transactionToAdd)),
+      totalExpense: updateExpense(account.transactions, transactionToAdd, "ADD_TRANSACTION"),
+      currentBalance: updateBalance2(account.transactions, transactionToAdd, "ADD_TRANSACTION")
       // currentBalance: Number(account.currentBalance) + Number(updateBalance(transactionToAdd))
     }
     //false - return account obj
@@ -94,6 +137,8 @@ export const deleteTransaction = (budgetState, transactionIdToDelete) => {
     ? {...account, 
       transactions: removeItemFromArray(account.transactions, transactionIdToDelete),
       totalIncome: updateIncome(account.transactions, transactionIdToDelete, "DELETE_TRANSACTION"),
+      totalExpense: updateExpense(account.transactions, transactionIdToDelete, "DELETE_TRANSACTION"),
+      currentBalance: updateBalance2(account.transactions, transactionIdToDelete, "DELETE_TRANSACTION")
     }
     //false - return account obj
     : account
